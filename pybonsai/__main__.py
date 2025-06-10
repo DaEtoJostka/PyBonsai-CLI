5b779669f14d85d88649a3a6fdf173e1a12cdd60
DaEtoJostka <karlovivan679@gmail.com>
2025-06-10 17:25:20 +0500

    update readme

--- pybonsai/__main__.py
+++ pybonsai/__main__.py
@@ -34,7 +34,7 @@ from math import radians
 from os import get_terminal_size
 
 
-VERSION = "1.2.5"
+VERSION = "1.2.6"
 DESC = "PyBonsai procedurally generates ASCII art bonsai trees in your terminal."
 
 
