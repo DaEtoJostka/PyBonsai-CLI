e0636cea4588e3f06101a7ec9f21d643f8e439e5
DaEtoJostka <karlovivan679@gmail.com>
2026-03-08 01:51:05 +0500

    doc: Change version to 3.0.0

--- pybonsai/metadata.py
+++ pybonsai/metadata.py
@@ -1,6 +1,6 @@
 """Package metadata shared across CLI entry points."""
 
-VERSION = "2.2.0"
+VERSION = "3.0.0"
 DESCRIPTION = (
     "PyBonsai procedurally generates ASCII art bonsai trees in your terminal.\n"
     "Leave a star on GitHub: https://github.com/DaEtoJostka/PyBonsai-CLI"
